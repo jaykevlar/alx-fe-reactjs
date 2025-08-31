f7a72b6fa31b32dfc547ef647ac9a27ffe23ea03
Jason <jaykevlar860@gmail.com>
2025-08-31 21:00:57 +0200

    Task 2: Add Header, MainContent, Footer components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import WelcomeMessage from './components/WelcomeMessage';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -18,8 +21,13 @@ function App() {
         </a>
       </div>
 
-      {/* WelcomeMessage Component */}
+      {/* Task 1 */}
       <WelcomeMessage />
+
+      {/* Task 2 */}
+      <Header />
+      <MainContent />
+      <Footer />
 
       <h1>Vite + React</h1>
       <div className="card">
