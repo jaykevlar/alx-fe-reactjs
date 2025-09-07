e436a3608f61061693f8c4fec1281fc82dbfb061
Jason <jaykevlar860@gmail.com>
2025-09-07 22:10:41 +0200

    Task 0: Applied inline CSS styling to all components in alx-react-app-new

---
+++ alx-react-app-new/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: '#222', color: '#fff', textAlign: 'center', padding: '15px', marginTop: '20px' }}>
+      <p>© 2025 My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
