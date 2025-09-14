9b2434a6d1f588163500b7d180604ec3db732bb7
Jason <jaykevlar860@gmail.com>
2025-09-14 22:43:16 +0200

    Refactor prop drilling: implement Context API

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import Counter from './components/Counter';
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
 
 function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
   return (
-    <div>
-      <h1>Simple Counter App</h1>
-      <Counter />
-    </div>
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
   );
 }
 
