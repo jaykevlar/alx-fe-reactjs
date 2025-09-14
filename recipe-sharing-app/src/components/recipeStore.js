6d4701527142aad17124f96173a6cec982064e08
Jason <jaykevlar860@gmail.com>
2025-09-14 22:00:22 +0200

    Fix: Add setRecipes action to recipeStore

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,10 +2,15 @@ import { create } from "zustand";
 
 const useRecipeStore = create((set) => ({
   recipes: [],
+
+  // Add a new recipe
   addRecipe: (recipe) =>
     set((state) => ({
       recipes: [...state.recipes, recipe],
     })),
+
+  // Replace the recipes array entirely
+  setRecipes: (recipes) => set({ recipes }),
 }));
 
 export default useRecipeStore;
