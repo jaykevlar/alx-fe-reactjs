f3218b5ea55331dadb254becdecfda31abb91052
Jason <jaykevlar860@gmail.com>
2025-09-14 22:13:56 +0200

    Task 1: Add React Router setup with Routes and RecipeDetails routing

--- recipe-sharing-app/src/components/App.jsx
+++ recipe-sharing-app/src/components/App.jsx
@@ -5,28 +5,32 @@ import RecipeList from "./components/RecipeList";
 import RecipeDetails from "./components/RecipeDetails";
 import SearchBar from "./components/SearchBar"; // Optional for Task 2
 
-const App = () => (
-  <Router>
-    <div>
-      <h1>Recipe Sharing App</h1>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <SearchBar />
-              <AddRecipeForm />
-              <RecipeList />
-            </>
-          }
-        />
-        <Route path="/recipe/:id" element={<RecipeDetailsWrapper />} />
-      </Routes>
-    </div>
-  </Router>
-);
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <h1>Recipe Sharing App</h1>
+        <Routes>
+          {/* Main page: add + search + list */}
+          <Route
+            path="/"
+            element={
+              <>
+                <SearchBar />
+                <AddRecipeForm />
+                <RecipeList />
+              </>
+            }
+          />
+          {/* Recipe details page */}
+          <Route path="/recipe/:id" element={<RecipeDetailsWrapper />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
-// Wrapper to pass ID param
+// Wrapper to pass ID param to RecipeDetails
 const RecipeDetailsWrapper = () => {
   const { id } = useParams();
   return <RecipeDetails recipeId={Number(id)} />;
